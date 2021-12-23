615f38dd442c34c30a7a90b9773f9d50010aac4a
Brayan Bermudez <bfbermudezc@gmail.com>
2021-12-23 11:19:38 -0500

    implementacion: > -se implementa dos nuevas clases para el consumo donde se realiza el consumo por medio de la libreria de retrofit

---
+++ app/src/main/java/com/example/app_movie_gd/rest/ApiAdapter.java
@@ -0,0 +1,29 @@
+package com.example.aflore_prueba.rest;
+
+import com.example.aflore_prueba.interfaces.rest.JsonApi;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiAdapter {
+
+    private ApiAdapter() {
+    }
+
+    private static JsonApi jsonApi;
+
+    public static JsonApi getDataMovie() {
+        if (jsonApi == null) {
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .client(new OkHttpClient().newBuilder().build())
+                    .baseUrl(Endpoints.URL_BASE)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            jsonApi = retrofit.create(JsonApi.class);
+        }
+        return jsonApi;
+    }
+
+}
---
+++ app/src/main/java/com/example/app_movie_gd/rest/Endpoints.java
@@ -0,0 +1,7 @@
+package com.example.aflore_prueba.rest;
+
+public class Endpoints {
+    public static final String URL_BASE = "https://api.themoviedb.org/3/movie/";
+    public static final String GET_MOVIE = "popular?api_key=0d677b16a44d2b5a79edf325bc1ee9b7&language=en-US&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+    public static final String URL_BASE_IMAGE = "https://image.tmdb.org/t/p/w500";
+}
